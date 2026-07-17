4ca0ea3292ab144bce7b5ff2b85ac4909a172f27
skhachatryan <skhachatryan@griddynamics.com>
2026-07-17 16:08:22 +0400

    delete igress in terraform

--- terraform/modules/helm_app/ingress.tf
+++ terraform/modules/helm_app/ingress.tf
@@ -14,33 +14,33 @@ resource "helm_release" "apex_app_ingress" {
   }
 }
 
-resource "kubernetes_ingress_v1" "app_routing" {
-  metadata {
-    name      = "apex-app-routing"
-    namespace = "apex-app"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-    }
-  }
+# resource "kubernetes_ingress_v1" "app_routing" {
+#   metadata {
+#     name      = "apex-app-routing"
+#     namespace = "apex-app"
+#     annotations = {
+#       "kubernetes.io/ingress.class" = "nginx"
+#     }
+#   }
 
-  spec {
-    rule {
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
+#   spec {
+#     rule {
+#       http {
+#         path {
+#           path      = "/"
+#           path_type = "Prefix"
 
-          backend {
-            service {
-              # The name of the Kubernetes Service exposing the website pods
-              name = "apex-frontend-svc"
-              port {
-                number = 80 # The port Service is listening on
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+#           backend {
+#             service {
+#               # The name of the Kubernetes Service exposing the website pods
+#               name = "apex-frontend-svc"
+#               port {
+#                 number = 80 # The port Service is listening on
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
